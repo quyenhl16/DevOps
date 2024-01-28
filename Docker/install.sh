2d4d2c28a4a441427f72448f6f9892377449c6e7
QuyenHL <halongquyen@gmail.com>
2024-01-28 13:43:25 +0000

    update scipt install docker on centos

---
+++ Docker/install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+## Remove the old docker
+sudo yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+
+## Set up the repository
+
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+## To install the latest version
+
+sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+## Start Docker
+
+sudo systemctl start docker
+
+## Run Docker hello-world
+
+sudo docker run hello-world
